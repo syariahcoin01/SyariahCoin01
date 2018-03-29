f07b2cfe1fa28f94257e4b0b19022aae2c5b4f1d
root <root@syariahcoin01.server>
2018-03-29 17:14:40 +0700

    changesportcointime

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #LearnCoinTEST3\r");
-            Send(hSocket, "WHO #LearnCoinTEST3\r");
+            Send(hSocket, "JOIN #SyariahCoin01TEST3\r");
+            Send(hSocket, "WHO #SyariahCoin01TEST3\r");
         } else {
-            // randomly join #LearnCoin00-#LearnCoin99
+            // randomly join #SyariahCoin0100-#SyariahCoin0199
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #LearnCoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #LearnCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #SyariahCoin01%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #SyariahCoin01%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
